6f3f739091ba4b28c37a3d694b21aee19bcc9007
Gabriel J Canavesio <gabrielcanavesio17@gmail.com>
2024-08-26 20:18:45 -0300

    Creación del proyecto java.

---
+++ TPdeso2024/src/main/java/isi/deso/tpdeso2024/TPdeso2024.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package isi.deso.tpdeso2024;
+
+/**
+ *
+ * @author gabic
+ */
+public class TPdeso2024 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
