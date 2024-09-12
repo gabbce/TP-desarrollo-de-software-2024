5504e40fc9746acc12ad8cfff55bf76ddf34fafc
Augusto Duarte <augusto.duarte.ok@gmail.com>
2024-09-12 19:43:01 -0300

    Primeras cosas etapa 3, declaradas el Dao, Memory, y la Excepcion

---
+++ TPdeso2024/src/main/java/isi/deso/tpdeso2024/ItemNoEncontradoExcepcion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package isi.deso.tpdeso2024;
+
+/**
+ *
+ * @author augus
+ */
+public class ItemNoEncontradoExcepcion extends Exception{
+    public ItemNoEncontradoExcepcion(){
+    super();
+    //System.out.println("Item no encontrado"); //mensaje a consola
+    }
+}
---
+++ TPdeso2024/src/main/java/isi/deso/tpdeso2024/ItemsPedidoDao.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package isi.deso.tpdeso2024;
+
+/**
+ *
+ * @author augus
+ */
+public interface ItemsPedidoDao {
+    //cambiar si corresponde!
+    /*public void Filtrado()throws ItemNoEncontradoExcepcion;
+    public void Filtrado()throws ItemNoEncontradoExcepcion;
+    public void Filtrado()throws ItemNoEncontradoExcepcion;
+    public void Filtrado()throws ItemNoEncontradoExcepcion;*/
+}
