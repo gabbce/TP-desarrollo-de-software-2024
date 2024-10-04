49703032c03a6d9db83498312f6b378e9435e610
JPDechFacu <jdechiara@frsf.edu.ar>
2024-10-04 10:55:18 -0300

    Se agregaron la clases Pedido y Pago(super de Tranferencia y MercadoPago)

---
+++ TPdeso2024/src/main/java/isi/deso/tpdeso2024/Pedido.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package isi.deso.tpdeso2024;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Pedido {
+    private PedidoDetalle detalle;
+    private Pago pago;
+
+    public Pedido(PedidoDetalle detalle, Pago pago) {
+        this.detalle = detalle;
+        this.pago = pago;
+    }
+
+    public PedidoDetalle getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(PedidoDetalle detalle) {
+        this.detalle = detalle;
+    }
+
+    public Pago getPago() {
+        return pago;
+    }
+
+    public void setPago(Pago pago) {
+        this.pago = pago;
+    }
+    
+    
+}
