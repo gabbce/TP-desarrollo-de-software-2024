d97ee9e64502341c033a2aad6cdaf2987e46507b
Augusto Duarte <augusto.duarte.ok@gmail.com>
2024-11-14 12:43:52 -0300

    avance en dao del item menu, pero le falta

--- TPdeso2024/src/main/java/isi/deso/tpdeso2024/daos/ItemMenuSQLDAO.java
+++ TPdeso2024/src/main/java/isi/deso/tpdeso2024/daos/ItemMenuSQLDAO.java
@@ -16,9 +16,264 @@ import java.util.List;
  */
 public class ItemMenuSQLDAO implements ItemMenuDAO {
 
+    private DataBaseConnection conector;
+
     public ItemMenuSQLDAO() {
+        conector = new DataBaseConnection();
     }
 
+
+	@Override
+	public boolean crear(ItemMenu v){
+		if(v.esComida()){
+			return crear((Plato)v);
+		}
+		return crear((Bebida)v);
+    }
+	private boolean crear(Plato v){
+		try {
+            this.conector.conectar();
+
+            PreparedStatement preparedStatement = conector.con.prepareStatement("""
+                                                                            INSERT INTO ItemMenu (es_comida,nombre,descripcion,id_vendedor,id_categoria,precio,peso,calorias,apto_celiaco,apto_vegano) 
+                                                                            VALUES (?,?,?,?,?,?,?,?,?,?);""");
+
+            //setear valores
+            preparedStatement.setBoolean(1, true);
+            preparedStatement.setString(2, v.getNombre());
+            preparedStatement.setString(3, v.getDescripcion());
+            preparedStatement.setInt(4, v.getVendedor().getId());
+			preparedStatement.setInt(5, v.getCategoria().getId());
+			preparedStatement.setFloat(6, v.getPrecio());
+			preparedStatement.setFloat(7, v.peso());
+			preparedStatement.setFloat(8, v.getCalorias());
+			preparedStatement.setBoolean(9, v.aptoCeliaco());
+			preparedStatement.setBoolean(10, v.aptoVegano());
+			
+			
+
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+            this.conector.cerrar();
+        } catch (Exception e) {
+            System.out.println("excepcion en " + this.getClass().getName() + ".create() " + e.getMessage());
+            return false;
+        }
+        return true;
+	}
+    private boolean crear(Bebida v){
+		try {
+            this.conector.conectar();
+
+            PreparedStatement preparedStatement = conector.con.prepareStatement("""
+                                                                            INSERT INTO ItemMenu (es_comida,nombre,descripcion,id_vendedor,id_categoria,precio,graduacion_alcoholica,tam) 
+                                                                            VALUES (?,?,?,?,?,?,?,?);""");
+
+            //setear valores
+            preparedStatement.setBoolean(1, false);
+            preparedStatement.setString(2, v.getNombre());
+            preparedStatement.setString(3, v.getDescripcion());
+            preparedStatement.setInt(4, v.getVendedor().getId());
+			preparedStatement.setInt(5, v.getCategoria().getId());
+			preparedStatement.setFloat(6, v.getPrecio());
+			preparedStatement.setFloat(7, v.getGraduacionAlcoholica());
+			preparedStatement.setFloat(8, v.getTam());
+			
+			
+
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+            this.conector.cerrar();
+        } catch (Exception e) {
+            System.out.println("excepcion en " + this.getClass().getName() + ".create() " + e.getMessage());
+            return false;
+        }
+        return true;
+	}
+
+    @Override
+    public boolean eliminar(int id) {
+        try {
+            this.conector.conectar();
+
+            PreparedStatement preparedStatement = conector.con.prepareStatement("""
+		DELETE FROM ItemMenu WHERE id = ?;
+		""");
+
+            //setear valores
+            preparedStatement.setInt(1, id);
+
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+            this.conector.cerrar();
+        } catch (Exception e) {
+            System.out.println("excepcion en " + this.getClass().getName() + ".eliminar() " + e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+
+	//falta de aca para abajo
+	//agregar metodos para crear los objetos, segun sea plato o bebida
+	//para devolver upcastear y meter en la List
+    @Override
+    public List<ItemMenu> listar() {
+        try {
+            this.conector.conectar();
+
+            PreparedStatement preparedStatement = conector.con.prepareStatement("""
+		SELECT * FROM ItemMenu;								
+		""");
+
+            ArrayList<ItemMenu> ret = new ArrayList<>();
+            ResultSet resultados = preparedStatement.executeQuery();
+            while (resultados.next()) {
+                ItemMenu tmp = new ItemMenu(
+                        resultados.getInt(1),
+                        resultados.getString(2),
+                        resultados.getString(3),
+                        new Coordenada(
+                                resultados.getFloat(4),
+                                resultados.getFloat(5)
+                        )
+                );
+                ret.add(tmp);
+            }
+
+            preparedStatement.close();
+            this.conector.cerrar();
+
+            return ret;
+        } catch (Exception e) {
+            System.out.println("excepcion en " + this.getClass().getName() + ".listar() " + e.getMessage());
+            return null;
+        }
+
+    }
+
+    @Override
+    public boolean actualizar(ItemMenu v) {
+        try {
+            this.conector.conectar();
+
+            PreparedStatement preparedStatement = conector.con.prepareStatement("""
+			UPDATE ItemMenu
+			SET nombre = ?, direccion = ?, latitud= ?, longitud = ?
+			WHERE id = ?
+			
+			""");
+
+            //setear valores
+            preparedStatement.setString(1, v.getNombre());
+            preparedStatement.setString(2, v.getDireccion());
+            preparedStatement.setDouble(3, v.getCoordenadas().getLatitud());
+            preparedStatement.setDouble(4, v.getCoordenadas().getLongitud());
+            preparedStatement.setInt(5, v.getId());
+
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+            this.conector.cerrar();
+        } catch (Exception e) {
+            System.out.println("excepcion en " + this.getClass().getName() + ".actualizar() " + e.getMessage());
+            return false;
+        }
+
+        return true;
+    }
+
+    public ItemMenu buscarPorID(int id) throws ItemMenuNoEncontradoException {
+        try {
+            this.conector.conectar();
+
+            PreparedStatement preparedStatement = conector.con.prepareStatement("""
+		SELECT * FROM ItemMenu WHERE id = ?;								
+		""");
+
+            //setear valores
+            preparedStatement.setInt(1, id);
+
+            ItemMenu ret = null;
+            ResultSet resultados = preparedStatement.executeQuery();
+            int contador = 0;
+            while (resultados.next()) {
+                ret = new ItemMenu(
+                        resultados.getInt(1),
+                        resultados.getString(2),
+                        resultados.getString(3),
+                        new Coordenada(
+                                resultados.getFloat(4),
+                                resultados.getFloat(5)
+                        )
+                );
+                contador++;
+            }
+            if (contador == 0) {
+                throw new ItemMenuNoEncontradoException("No existe vendedor con id " + id);
+            }
+
+            preparedStatement.close();
+            this.conector.cerrar();
+            
+            return ret;
+            
+        } catch (Exception e) {
+            System.out.println("excepcion en " + this.getClass().getName() + ".buscarPorID() " + e.getMessage());
+            return null;
+        }
+        
+    }
+
+    @Override
+    public List<ItemMenu> buscar(String nombre) {
+		throw new UnsupportedOperationException("Hay que acomodar la interfaz dao");
+	}
+    
+	
+	
+	//completar
+	public List<ItemMenu> buscar(String nombre) throws ItemMenuNoEncontradoException{
+		/*buscar por nombre,Categoria.tipo, id_vendedor
+		Se me ocurrio hacer busqueda por un criterio y luego pasar al proximo 
+		llevando un arreglo con los resultados, y lo vas recortando 
+		*/
+        try {
+            this.conector.conectar();
+
+            PreparedStatement preparedStatement = conector.con.prepareStatement("""
+		SELECT * FROM ItemMenu WHERE nombre LIKE ?;								
+		""");
+
+            //setear valores
+            preparedStatement.setString(1, "%" + nombre + "%");
+
+            ArrayList<ItemMenu> ret = new ArrayList<>();
+            ResultSet resultados = preparedStatement.executeQuery();
+            while (resultados.next()) {
+                ItemMenu tmp = new ItemMenu(
+                        resultados.getInt(1),
+                        resultados.getString(2),
+                        resultados.getString(3),
+                        new Coordenada(
+                                resultados.getFloat(4),
+                                resultados.getFloat(5)
+                        )
+                );
+                ret.add(tmp);
+            }
+
+            preparedStatement.close();
+            this.conector.cerrar();
+
+            return ret;
+        } catch (Exception e) {
+            System.out.println("excepcion en " + this.getClass().getName() + ".buscar() " + e.getMessage());
+            return null;
+        }
+    }
+	
+	
+	//estos no se usan
     @Override
     public ArrayList<ItemMenu> Filtrado(String criterio, String valor) throws ItemNoEncontradoExcepcion {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
@@ -35,33 +290,7 @@ public class ItemMenuSQLDAO implements ItemMenuDAO {
     }
 
     @Override
-    public ArrayList<ItemMenu> BuscarPorRestaurante(int idVendedor) throws ItemNoEncontradoExcepcion {
+    public ArrayList<ItemMenu> BuscarPorRestaurante(int idItemMenu) throws ItemNoEncontradoExcepcion {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public boolean crear(ItemMenu it) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public boolean eliminar(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public boolean actualizar(ItemMenu it) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public List<ItemMenu> listar() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public List<ItemMenu> buscar(String nombre) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-    
+    }    
 }
